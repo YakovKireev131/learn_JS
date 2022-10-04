0ab768faa738bf5eef17167ef1ef71feae363e50
YakovKireev131 <caarlos131@yandex.ru>
2022-10-04 07:57:14 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+
+var a = 1;
+let b = 230;
+const PI = 3.14;
+let st = 'hdhf'
+
+// console.log(typeof(a));
+// console.log(typeof(st));
+// console.log(typeof(null));
+// console.log(typeof(true));
+// console.log(typeof(undefined));
+
+// let c = 12;
+// let d = String(c);
+// let f = toString(13);
+// console.log(typeof(d));
+// console.log(typeof(f));
+// console.log(typeof(Number('55')));
+// console.log(typeof(+st));
+// console.log(b.toString());
+// console.log(b.toString(16));
+// console.log(b.toString(2));
+// console.log(Math.floor(3.3));
+// console.log(typeof(NaN));
+// console.log(typeof(Infinity));
+// console.log(Math.trunc(2.2));
+// console.log(PI.toFixed(1));
+// console.log(isNaN(NaN));
+// console.log(isNaN(a));
+// console.log(isNaN('NaN'));
+// console.log(parseInt('100px'));
+// console.log(parseInt('2.3'));
+// console.log(parseInt('px111'));
+// console.log(parseInt('100px', 2));
+// console.log(parseFloat('100.3px'));
+// console.log(parseFloat('100.01px.1'));
+// console.log(Number(Math.random().toFixed(3)));
+// console.log(Math.max(1, 3, 0, -3, 99));
+// console.log(Math.min(1, 3, 0, -3, 99));
+// console.log(Math.pow(3, 4));
+// let arr = ['str', 22, true];
+// console.log(arr[1]);
+// alert(arr[1]);
+
+// // let x = confirm('Are you here?');
+// // console.log(x);
+
+// let z = prompt('Age?', '0');
+// console.log('Мне ' + z + ' лет');
+// console.log(typeof(+z));
+let i = 0;
+console.log(5==='5');
