300fef67e2c6445d57bf81e168a2b08eefb114a8
YakovKireev131 <caarlos131@yandex.ru>
2022-10-15 14:42:38 +0300

    Прошел циклы

---
+++ stage_2/zikl.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// let a = 30;
+// while (a < 36) {
+//     console.log(a);
+//     a++;
+// }
+
+// let b = 10;
+// do {
+//     console.log(b);
+//     b--;
+// } while (b > 0);
+// let i;
+// for (let i = 0; i < 5; i++) {
+//     console.log('Число', i);
+// }
+
+for (let i = 0; i < 10; i++){
+    if (i == 6 || i == 8) continue;
+    console.log(i);
+}
